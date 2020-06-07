c0a60cb3e127df331a3c2756aaf690c7e15a8f1d
albinperssonorangegrove <66575869+albinperssonorangegrove@users.noreply.github.com>
2020-06-07 16:50:06 -0700

    Updates

---
+++ precache-manifest.7ac3bacdb9d40691658ad2aaf8ea2677.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2252b8092d5b7b4d8d55569ccfbdc3c",
+    "url": "/farm/index.html"
+  },
+  {
+    "revision": "3441a1527f5bec6b5e3b",
+    "url": "/farm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca4f6208a3fc795cbbd2",
+    "url": "/farm/static/js/2.e3883bac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/farm/static/js/2.e3883bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3441a1527f5bec6b5e3b",
+    "url": "/farm/static/js/main.07a94ad7.chunk.js"
+  },
+  {
+    "revision": "bf5ce9b0ee7b6484afe2",
+    "url": "/farm/static/js/runtime-main.7a05eae1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/farm/static/media/logo.5d5d9eef.svg"
+  }
+]);
